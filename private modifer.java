579f2631859ff8e98c9a023efa87d24d7185dee8
yanamalaumesh1874 <62681339+yanamalaumesh1874@users.noreply.github.com>
2020-04-01 20:38:08 +0530

    Create private modifer.java

---
+++ private modifer.java
@@ -0,0 +1,13 @@
+class carz 
+{
+	private String fname="ford";
+	private String lname="mustang";
+	private String sportscar="yes";
+	public static void main(String[] args) 
+	{
+		carz c=new carz();
+		System.out.println(c.fname+" "+c.lname);
+		System.out.println(c.sportscar);
+
+	}
+}
